3d3f9ce7cf2e2fc79201d59105e4324fe9472c99
jai-atithya <jaiatithya@gmail.com>
2025-06-13 18:43:29 +0530

    sidebar work starting app.jsx file error resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Sidebar } from "./components/Sidebar"
+import { CourseCard } from "./components/coursemanagement/CourseCard"
 import { NotFound } from './pages/NotFound';
 import './App.css'
 
@@ -11,6 +13,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Sidebar/>}></Route>
         <Route path="/coursecard" element={<CourseCard/>}></Route>
       </Routes>
     </BrowserRouter>
