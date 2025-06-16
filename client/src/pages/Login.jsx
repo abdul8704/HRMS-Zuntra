378e60a2e8699ccbff9932b9cf74a217c92a9007
jai-atithya <jaiatithya@gmail.com>
2025-06-16 14:13:22 +0530

    login.jsx add

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
-
+import zuntraLogo from "../assets/zuntra.png"
 export const Login = () => {
     return (
         <div>
+            <img className="login-logo-container" src={zuntraLogo} alt="ZUNTRA"/>
             <div className="login-container">
-                <div className="login-logo-container">
-                    dfasf
-                </div>
-                <div>
-
+                <div className="login-card">
+                    112
                 </div>
             </div>
             <style jsx>{`
                 .login-container {
                     display: flex;
                     flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
                     height: 100vh;
                     width: 100vw;
-                    background-color: #00ffff;
+                    // background-color: #00ffff;
                 }
                 .login-logo-container{
                     position: absolute;
-                    width: auto;
-                    height: auto;
-                    top: 0;
-                    left: 10px;
-                    background-color: #ff0000;
+                    width: 15vw;
+                    margin: 1rem;
+                    // background-color: #ff0000;
+                }
+                .login-card{
+                    width: 30vw;
+                    background-color: #00ff00;
                 }
             `}</style>
         </div>
