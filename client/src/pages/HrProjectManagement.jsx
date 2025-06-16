1909cc7c15f90d48aba64164cc59d0e2adc011e4
jai-atithya <jaiatithya@gmail.com>
2025-06-16 11:08:55 +0530

    Project Management Structure created

--- client/src/pages/HrProjectManagement.jsx
+++ client/src/pages/HrProjectManagement.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { Sidebar } from "../components/Sidebar"
+import { ProjectCard } from '../components/projectManagement/ProjectCard'
+import { ProjectNavbar } from '../components/projectManagement/ProjectNavbar'
+import { TaskCard } from '../components/projectManagement/TaskCard'
 export const HrProjectManagement = () => {
   return (
     <div className="website-container">
-    <Sidebar/>
-    <div className="website-module">
-      
-      <div className="project-cards-container">
-
+      <Sidebar />
+      <div className="website-module">
+        <ProjectNavbar />
+        <div className="project-cards-container">
+          <ProjectCard />
+          <TaskCard />
+        </div>
+        <style jsx>{`
+        .project-cards-container{
+          display: flex;
+          flex-wrap: wrap;
+          max-height: 100%;
+          overflow-y: auto;
+          overflow-x: hidden;
+        }
+      `}</style>
       </div>
     </div>
-</div>
   )
 }
