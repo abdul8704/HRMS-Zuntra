aeec9068df5a863747c4ff2d928ece346689e941
jai-atithya <jaiatithya@gmail.com>
2025-06-16 15:18:11 +0530

    login page updated a bit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,7 +6,12 @@ export const Login = () => {
             <img className="login-logo-container" src={zuntraLogo} alt="ZUNTRA"/>
             <div className="login-container">
                 <div className="login-card">
-                    112
+                    <h1 className="login-card-title">LOGIN</h1>
+                    <form>
+                        <input className="login-card-input" type="email" placeholder='Email'></input>
+                        <input className="login-card-input" type="password" placeholder='Password'></input>
+                        <label className="login-card-forgot">Forgot Password?</label>
+                    </form>
                 </div>
             </div>
             <style jsx>{`
@@ -26,8 +31,39 @@ export const Login = () => {
                     // background-color: #ff0000;
                 }
                 .login-card{
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
                     width: 30vw;
-                    background-color: #00ff00;
+                    border-radius: 1rem;
+                    box-shadow: 0 0 26px 0px rgba(0, 0, 0, 0.25);
+                    // background-color:rgb(255, 255, 255);
+                }
+                .login-card-title{
+                    margin: 2rem;
+                    font-weight: block;
+                }
+                .login-card form{
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: flex-end;
+                    padding: 0;
+                    width: 80%;
+                }
+                .login-card-input{
+                    width: 90%;
+                    font-size: 1rem;
+                    padding: 1rem;
+                    margin-top: 1rem;
+                    border-radius: 0.5rem;
+                    border: 2px solid #000000;
+                }
+                .login-card-forgot{
+                    font-size: 0.8rem;
+                    margin: 0.2rem;
+                    color: #0000ff;
                 }
             `}</style>
         </div>
