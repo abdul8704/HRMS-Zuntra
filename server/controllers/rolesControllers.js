e5b738012636886b1c2e76319541071c18dd9725
abdul8704 <abdulaziz87049@gmail.com>
2025-06-17 15:31:52 +0530

    roles, meetings, tasks init done

---
+++ server/controllers/rolesControllers.js
@@ -0,0 +1,14 @@
+const rolesService = require('../services/rolesService')
+
+const getAllroles = async (req, res) => {
+    const roles = await rolesService.getAllRolesData();
+    if(roles.length === 0){
+        res.status(404)
+        throw new Error("No roles found");
+    }
+    return res.status(200).json(roles);
+};
+
+module.exports = {
+    getAllroles,
+};
