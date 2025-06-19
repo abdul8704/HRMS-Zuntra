00c1ca5ad67d809158cd89b2dbfba75fc3faad70
jai-atithya <jaiatithya@gmail.com>
2025-06-19 15:46:04 +0530

    project management add

--- server/controllers/projectManagement.js
+++ server/controllers/projectManagement.js
@@ -1,18 +1,36 @@
-const asyncHandler=require('express-async-handler');
+const asyncHandler = require('express-async-handler');
 const courseService = require("../services/projectService")
 
 
 // @desc Get all ongoing projects
-// @route GET /api/project/current
-// @access HR
-const getAllCurrentProjects =asyncHandler(async(req,res) =>{
-    const projectsList = await courseService.getAllCurrentProjects();
-    if(projectsList.length===0){
+// @route GET /api/project/ongoing
+const getAllOnGoingProjects = async (req, res) => {
+    const projectsList = await projectService.getAllOnGoingProjects();
+    if (projectsList.length === 0) {
         res.status(404);
-        throw new Error("No Projects Available")
+        throw new Error("No Projects Currently OnGoing")
     }
     console.log(projectsList)
-    res.status(200).json({message: "All Projects"});
-});
+    return res.status(200).json({
+        success: true,
+        data: projectsList,
+    });
+};
 
-module.exports = { getAllCurrentProjects };
+// @desc Get all finished projects
+// @route GET /api/project/finished
+const getAllFinishedProjects = async (req, res) => {
+    const projectsList = await projectService.getAllFinishedProjects();
+    if (projectsList.length === 0) {
+        res.status(404);
+        throw new Error("No Finished Projects Available")
+    }
+    console.log(projectsList)
+    res.status(200).json({
+        success: true,
+        data: projectsList,
+    });
+};
+
+
+module.exports = { getAllOnGoingProjects, getAllFinishedProjects };
