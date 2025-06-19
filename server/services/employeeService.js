33ce2478b6fe82c07b96aea5c976ed5f87dfeff7
jai-atithya <jaiatithya@gmail.com>
2025-06-19 20:42:45 +0530

    previous schema was imported so error... so just for the sake to solve error... just added proper import

--- server/services/employeeService.js
+++ server/services/employeeService.js
@@ -1,5 +1,5 @@
 const Attendance = require("../models/attendance")
-const User = require("../models/userDetails.js")
+const User = require("../models/userCredentials.js")
 
 const getAttendaceByUserId = async (userid, startDate, endDate) => {
     try {
--- server/services/user.js
+++ server/services/user.js
@@ -1,4 +1,4 @@
-const UserDetails = require('../models/userDetails');
+const UserCredentials = require('../models/userCredentials');
 
 const getAllUserDetails = async () => {
     const userData = await UserDetails.find({});
