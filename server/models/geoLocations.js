be4599d43e8c8b872723ab7ff44d538eca8961d0
abdul8704 <abdulaziz87049@gmail.com>
2025-06-23 20:50:24 +0530

    models for geo fencing done

---
+++ server/models/geoLocations.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const geoLocationSchema = new mongoose.Schema({
+    latitude: {
+        type: Number,
+        required: true
+    },
+    longitude: {
+        type: Number,
+        required: true
+    },
+    campusName: {
+        type: String,
+        required: true
+    },
+    radius: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('GeoLocation', geoLocationSchema);
