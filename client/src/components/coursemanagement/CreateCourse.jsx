78d2ea5e1ca7510f95dfda08b207c3b34ff3281c
Keerthika <nkeerthika09@gmail.com>
2025-06-25 20:01:55 +0530

    Idk what changes i made here, maybe worked with create course tho

--- client/src/components/coursemanagement/CreateCourse.jsx
+++ client/src/components/coursemanagement/CreateCourse.jsx
@@ -4,7 +4,7 @@ export const CreateCourse = () => {
     return (
         <>
             <div className="loom-section">
-                <h1>Record with Loom</h1>
+                <h1 className='loom-heading'>Record with Loom</h1>
                 <button className='loom-button'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" viewBox="0 0 40 40">
                         <path fill="#625DF5" d="M40 17.776H28.303l10.13-5.849-2.224-3.854-10.13 5.849 5.847-10.13-3.854-2.225-5.847 10.129V0h-4.45v11.697l-5.85-10.13-3.852 2.225 5.848 10.129-10.13-5.848-2.224 3.853 10.13 5.849H0v4.45h11.695L1.567 28.072l2.224 3.854 10.13-5.848-5.85 10.13 3.855 2.224 5.848-10.13V40h4.45V28.304l5.847 10.13 3.854-2.225-5.849-10.13 10.13 5.848 2.225-3.854-10.129-5.848h11.696v-4.45H40ZM20 26.05a6.074 6.074 0 1 1 0-12.148 6.074 6.074 0 1 1 0 12.148Z" />
@@ -19,39 +19,43 @@ export const CreateCourse = () => {
             </div>
             <style>
                 {
-                    `
+                `
                 .loom-section {
-                    height: calc(100vh - 80px);
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                    text-align: center;
-                    padding: 2rem;
+                height: calc(100vh - 80px);
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                text-align: center;
+                padding: 2rem;
                 }
 
-                .loom-section h1 {
-                    font-size: 2rem;
-                    font-weight: 600;
-                    margin-bottom: 2rem;
+                .loom-heading {
+                font-size: 2rem;
+                font-weight: 600;
+                margin-bottom: 2rem;
                 }
 
                 .loom-button {
-                    background-color:  #BBD3CC;
-                    border: none;
-                    padding: 0.75rem 1.5rem;
-                    margin: 0.5rem 0;
-                    border-radius: 1rem;
-                    font-size: 1rem;
-                    cursor: pointer;
-                    display: flex;
-                    align-items: center;
-                    gap: 0.5rem;
-                    transition: background-color 0.3s ease;
+                background-color: #A6C4BA;
+                padding: 0.75rem 1.5rem;
+                height: 3rem;
+                width: 18rem;
+                margin: 0.5rem 0;
+                border-radius: 1.5rem
+                font-size: 1rem;
+                color: #ffffff;
+                font-weight: 700;
+                cursor: pointer;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                gap: 0.5rem;
+                transition: background-color 0.3s ease;
                 }
 
                 .loom-button:hover {
-                    background-color: #A6C4BA;
+                background-color:  #BBD3CC;
                 }
                 `
                 }
