5bbd66afbacf637857654308b85478842811d7b6
Harini <harinisankar1712@gmail.com>
2025-06-25 14:17:25 +0530

    Dashboard Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Login/>}></Route>
         <Route path="/newuser" element={<NewUser/>} />
-        <Route path="/dashboard" element={<HrDashboard/>}></Route>
+        <Route path="/dashboard" element={<DashBoard/>}></Route>
         <Route path="/projects" element={<HrProjectManagement/>}></Route>
         <Route path="/project/:navId" element={<HrProjectDetails/>}></Route>
         <Route path="/employee/:navId" element={<HrEmployeeManagement/>}></Route>
