5d44002737cce29858fc1e496f7cfce5c34d7ae8
aadhijah <aadhijah11494@gmail.com>
2025-06-27 07:42:01 +0530

    add course form

--- client/src/components/coursemanagement/AddModuleButtons.jsx
+++ client/src/components/coursemanagement/AddModuleButtons.jsx
@@ -5,13 +5,13 @@ const AddModuleButtons = ({ onAddModule, onAddSubModule }) => {
     <div className="flex gap-4 mt-4">
       <button
         onClick={onAddModule}
-        className="bg-gray-200 text-gray-700 px-6 py-2 rounded w-48 hover:bg-gray-300"
+        className="bg-gray-500 text-white px-6 py-2 rounded w-48 hover:bg-gray-600"
       >
         Add Module
       </button>
       <button
         onClick={onAddSubModule}
-        className="bg-gray-200 text-gray-700 px-6 py-2 rounded w-48 hover:bg-gray-300"
+        className="bg-gray-500 text-white px-6 py-2 rounded w-48 hover:bg-gray-600"
       >
         Add Sub Module
       </button>
