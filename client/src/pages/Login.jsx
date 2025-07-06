640a8da53db4c57ab09d7d734fc2e7e2228fb88d
Keerthika <nkeerthika09@gmail.com>
2025-07-06 14:28:12 +0530

    Login eye icon added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,6 +19,8 @@ export const Login = () => {
     name: '', email: '', phone: '', password: '', confirmPassword: '', otp: ''
   });
 
+  const [showPassword, setShowPassword] = useState(true);
+
   const handleToggle = () => {
     setIsSignup(!isSignup);
     setShowReset(false);
@@ -26,7 +28,7 @@ export const Login = () => {
   };
 
   const validateEmail = (email) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    return /^[^\s@]+@[^@\s]+\.(com|in)$/.test(email);
   };
 
   const handleSignupChange = (e) => {
@@ -39,7 +41,6 @@ export const Login = () => {
       if (name === "email" && value && !validateEmail(value)) {
         updated.email = "Enter a valid email.";
       }
-
       if (name === "confirmPassword" && value !== signupData.password) {
         updated.confirmPassword = "Passwords do not match.";
       }
@@ -51,7 +52,16 @@ export const Login = () => {
   const handleLoginChange = (e) => {
     const { name, value } = e.target;
     setLoginData(prev => ({ ...prev, [name]: value }));
-    setFormErrors(prev => ({ ...prev, [name]: '' }));
+
+    setFormErrors(prev => {
+      const updated = { ...prev, [name]: '' };
+
+      if (name === "email" && value && !validateEmail(value)) {
+        updated.email = "Enter a valid email.";
+      }
+
+      return updated;
+    });
   };
 
   const handleResetChange = (e) => {
@@ -330,10 +340,35 @@ export const Login = () => {
                           <input name="email" value={loginData.email} onChange={handleLoginChange} className="login-input" type="email" placeholder="Email" />
                           {formErrors.email && <p className="login-error-text">{formErrors.email}</p>}
                         </div>
-                        <div>
-                          <input name="password" value={loginData.password} onChange={handleLoginChange} className="login-input" type="password" placeholder="Password" />
+                        <div style={{ position: 'relative' }}>
+                          <input
+                            name="password"
+                            value={loginData.password}
+                            onChange={handleLoginChange}
+                            className="login-input"
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Password"
+                            required
+                          />
 
+                          <div
+                            className="login-password-toggle"
+                            onClick={() => setShowPassword(!showPassword)}
+                            role="button"
+                            aria-label={showPassword ? "Hide password" : "Show password"}
+                          >
+                            {showPassword ? (
+                              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="rgba(32, 30, 30, 0.82)">
+                                <path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Z" />
+                              </svg>
+                            ) : (
+                              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="rgba(32, 30, 30, 0.82)">
+                                <path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z" />
+                              </svg>
+                            )}
+                          </div>
                         </div>
+
                       </div>
                       <div className="login-forgot-container">
                         {formErrors.password && <p className="login-error-text flex-[1]">{formErrors.password}</p>}
@@ -468,6 +503,7 @@ export const Login = () => {
     border: 1px solid #aaa;
     background-color: #fff;
     box-sizing: border-box;
+    position: relative;
   }
 
   .login-input:focus {
@@ -480,9 +516,25 @@ export const Login = () => {
     color: #888;
   }
 
+  .login-password-toggle {
+  position: absolute;
+  right: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+  color:rgb(15, 15, 16);
+  width: 23px;
+  height: 23px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  padding: 2px;
+}
+
   .login-forgot-container {
     display:flex;
-    // background-color: rgba(255, 32, 147, 0.44);
     justify-content: flex-end;
     align-items: center;
     width: 100%;
