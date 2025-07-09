60c1871eb8085734f20d1c6ad569f408040f73c6
Keerthika <nkeerthika09@gmail.com>
2025-07-09 20:12:32 +0530

    Login 500 statusCode sus

--- server/errors/ApiError.js
+++ server/errors/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         super(message);
         this.statusCode = statusCode;
         if (details) this.details = details;
-
+        console.log("Api Error", message, statusCode, details);
         Error.captureStackTrace(this, this.constructor);
     }
 }
