bad1061d16949198362f48359639ac5ac8c92d48
Abish DM <abish.dm122@gmail.com>
2025-07-16 15:52:56 +0530

    employee course progress updated

--- client/src/pages/employee/EmployeeDetails.jsx
+++ client/src/pages/employee/EmployeeDetails.jsx
@@ -83,7 +83,11 @@ export const EmployeeDetails = ({ type }) => {
             {navId === "courses" && (
               <>
                 <p>Courses</p>
-                <EmployeeCourseProgress/>
+                <div className="h-[9rem] w-full sm:w-[24rem]">
+  <EmployeeCourseProgress />
+</div>
+
+
                 <button
                   className="fixed bottom-8 right-20 w-14 h-14 bg-[#BBD3CC] text-[#6c6c6c] rounded-full flex items-center justify-center text-2xl font-bold cursor-pointer hover:scale-110 hover:bg-[#A6C4BA] transition-transform z-[1000]"
                   onClick={() => setShowAssignCourse(true)}
