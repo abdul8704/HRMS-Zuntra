8a00848b9d1e2f5203c756483f810b48ba31919f
Keerthika <nkeerthika09@gmail.com>
2025-07-16 11:10:54 +0530

    EmpCourseProgress imported

--- client/src/pages/employee/EmployeeDetails.jsx
+++ client/src/pages/employee/EmployeeDetails.jsx
@@ -83,6 +83,7 @@ export const EmployeeDetails = ({ type }) => {
             {navId === "courses" && (
               <>
                 <p>Courses</p>
+                <EmployeeCourseProgress/>
                 <button
                   className="fixed bottom-8 right-20 w-14 h-14 bg-[#BBD3CC] text-[#6c6c6c] rounded-full flex items-center justify-center text-2xl font-bold cursor-pointer hover:scale-110 hover:bg-[#A6C4BA] transition-transform z-[1000]"
                   onClick={() => setShowAssignCourse(true)}
