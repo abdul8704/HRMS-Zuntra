d444dc417954139dad8823756a9513be70897b5b
Harini <harinisankar1712@gmail.com>
2025-07-18 11:00:10 +0530

    ceo Dash board

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import { Upskill } from './pages/course/Upskill';
 import { Attendance } from './pages/attendance/Attendance';
 import { HrPersonalAttendance } from './pages/HrPersonalAttendance';
 import { ProtectRoute } from './pages/ProtectRoute';
+import { CeoDashboard } from './pages/dashboard/CeoDashboard';
 import './App.css'
 import './index.css'
 
@@ -22,7 +23,7 @@ import './index.css'
 function App() {
   const [userid, setUserid] = useState('');
 
- return (
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
@@ -31,74 +32,79 @@ function App() {
           <ProtectRoute>
             <DashBoard />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/employee/:navId" element={
           <ProtectRoute>
             <EmployeeManagement />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/employee/:empId/details/:navId" element={
           <ProtectRoute>
             <EmployeeDetails type="user" />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/employee/role/:roleId/details" element={
           <ProtectRoute>
             <EmployeeDetails type="role" />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/courses/:navId" element={
           <ProtectRoute>
             <CourseManagement />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/course/:courseId/intro" element={
           <ProtectRoute>
             <CourseIntro />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/course/learn/:courseId" element={
           <ProtectRoute>
             <CourseLearn />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/projects/:navId" element={
           <ProtectRoute>
             {/* <HrProjectManagement /> */}
             <ProjectOverview />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/project/:projectId/:navId" element={
           <ProtectRoute>
             <HrProjectDetails />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/attendance/:navId" element={
           <ProtectRoute>
             <Attendance />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/attendance/" element={
           <ProtectRoute>
             <HrPersonalAttendance />
           </ProtectRoute>
-        }/>
+        } />
 
         <Route path="/upskill/:navId" element={
           <ProtectRoute>
             <Upskill />
           </ProtectRoute>
-        }/>
+        } />
+        <Route path="/ceo" element={
+          <ProtectRoute>
+            <CeoDashboard/>
+          </ProtectRoute>
+        } />
 
 
         {/* Not Found Route */}
