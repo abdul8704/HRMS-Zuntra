bb8745fa580bb9859b80dd15cf96871d95a8e651
Chandrika9906 <chandrika992006@gmail.com>
2025-07-28 10:54:33 +0530

    responsiveness

--- client/src/pages/attendance/Attendance.jsx
+++ client/src/pages/attendance/Attendance.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import { Sidebar } from '../../components/Sidebar';
@@ -11,81 +11,92 @@ import { AttendanceCard } from './components/AttendanceCard';
 import { LeaveForm } from './components/LeaveForm';
 import { ScheduleForm } from './components/ScheduleForm';
 import { SampleCard } from './components/samplecard';
-import  LeaveFormHistory  from './components/LeaveFormHistory';
-
+import LeaveFormHistory from './components/LeaveFormHistory';
 
 export const Attendance = () => {
     const { navId } = useParams();
+
     return (
-        <>
-            <div className="flex w-screen h-screen">
-                <Sidebar />
-                <div className="flex gap-[1rem] flex-col flex-1 p-[1rem] h-screen">
-                    <Navbar type="attendance" role="hr" />
-                    {navId === 'me' && (
-                    <div className="flex-1 bg-[#FFFFFF] grid grid-cols-8 grid-rows-8 gap-[1rem] overflow-hidden">
-                        {/* Time Card IN */}
-                        <div className="row-start-1 col-start-1 col-span-2 row-span-1 rounded-lg">
-                            <TimeCard state="in" time="09:20" showLabel={false} color={true} />
-                        </div>
-                        {/* Time Card Out */}
-                        <div className="row-start-2 col-start-1 col-span-2 row-span-1 rounded-lg">
-                            <TimeCard state="out" time="09:20" showLabel={false} color={true} />
-                        </div>
-                        {/* Time Card Work */}
-                        <div className="row-start-1 col-start-3 col-span-2 row-span-1 rounded-lg">
-                            <TimeCard state="work" time="09:20" showLabel={false} color={true} />
-                        </div>
-                        {/* Time Card Break */}
-                        <div className="row-start-2 col-start-3 col-span-2 row-span-1 rounded-lg">
-                            <TimeCard state="break" time="09:20" showLabel={false} color={true} />
-                        </div>
-                        {/* chart */}
-                        <div className="row-start-1 col-start-5 col-span-4 row-span-3 rounded-lg">
-                            <WorkBreakComposition />
-                        </div>
-                        {/* calendar */}
-                        <div className="row-start-3 col-start-1 col-span-4 row-span-6 rounded-lg">
-                            <AttendanceCalendar />
-                        </div>
-                        {/* attendance log */}
-                        <div className="row-start-4 col-start-5 col-span-4 row-span-5 rounded-lg">
-                            <AttendanceCard />
-                        </div>
-                    </div>
-                    )}
-                    {navId === "apply" && (
-                        <div className="flex flex-row w-full h-full overflow-hidden gap-[1rem]">
-                            <div className='flex-1'>
-                                <AttendanceCard />
+        <div className="flex flex-col lg:flex-row w-full h-screen overflow-hidden">
+            <Sidebar />
+            <div className="flex flex-col gap-4 flex-1 p-4 overflow-y-auto">
+                <Navbar type="attendance" role="hr" />
+
+                {/* ----- 'me' tab ----- */}
+                {navId === 'me' && (
+                    <>
+                        {/* Desktop/Laptop View (md and up) */}
+                        <div className="hidden md:grid flex-1 bg-[#FFFFFF] grid-cols-8 grid-rows-8 gap-[1rem] overflow-hidden">
+                            <div className="row-start-1 col-start-1 col-span-2 row-span-1 rounded-lg">
+                                <TimeCard state="in" time="09:20" showLabel={false} color={true} />
                             </div>
-                            <div className='flex-1 p-4 flex flex-col gap-[1rem]'>
-                                <div className="flex-[4] overflow-hidden">
-                                    <LeaveFormHistory />
-                                </div>
-                                <div className="flex-[6] overflow-hidden">
-                                    <LeaveForm />
-                                </div>
+                            <div className="row-start-2 col-start-1 col-span-2 row-span-1 rounded-lg">
+                                <TimeCard state="out" time="09:20" showLabel={false} color={true} />
                             </div>
-                        </div>
-                    )}
-                    {navId === "schedule" && (
-                        <div className="flex flex-row w-full h-full overflow-hidden gap-[1rem]">
-                            <div className='flex-1'>
+                            <div className="row-start-1 col-start-3 col-span-2 row-span-1 rounded-lg">
+                                <TimeCard state="work" time="09:20" showLabel={false} color={true} />
+                            </div>
+                            <div className="row-start-2 col-start-3 col-span-2 row-span-1 rounded-lg">
+                                <TimeCard state="break" time="09:20" showLabel={false} color={true} />
+                            </div>
+                            <div className="row-start-1 col-start-5 col-span-4 row-span-3 rounded-lg">
+                                <WorkBreakComposition />
+                            </div>
+                            <div className="row-start-3 col-start-1 col-span-4 row-span-6 rounded-lg">
                                 <AttendanceCalendar />
                             </div>
-                            <div className='flex-1 flex flex-col gap-[1rem]'>
-                                <div className='flex-1'>
-                                    <SampleCard />
-                                </div>
-                                <div className='flex-1'>
-                                    <ScheduleForm />
-                                </div>
+                            <div className="row-start-4 col-start-5 col-span-4 row-span-5 rounded-lg">
+                                <AttendanceCard />
                             </div>
                         </div>
-                    )}
-                </div>
+
+                        {/* Mobile View (below md) */}
+                        <div className="grid md:hidden gap-4">
+                            <TimeCard state="in" time="09:20" showLabel={false} color={true} />
+                            <TimeCard state="out" time="09:20" showLabel={false} color={true} />
+                            <TimeCard state="work" time="09:20" showLabel={false} color={true} />
+                            <TimeCard state="break" time="09:20" showLabel={false} color={true} />
+                            <WorkBreakComposition />
+                            <AttendanceCalendar />
+                            <AttendanceCard />
+                        </div>
+                    </>
+                )}
+
+                {/* ----- 'apply' tab ----- */}
+                {navId === 'apply' && (
+                    <div className="flex flex-col lg:flex-row w-full h-full gap-4 overflow-auto">
+                        <div className="w-full lg:w-1/2">
+                            <AttendanceCard />
+                        </div>
+                        <div className="w-full lg:w-1/2 flex flex-col gap-4">
+                            <div className="flex-1 overflow-hidden">
+                                <LeaveFormHistory />
+                            </div>
+                            <div className="flex-1 overflow-hidden">
+                                <LeaveForm />
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {/* ----- 'schedule' tab ----- */}
+                {navId === 'schedule' && (
+                    <div className="flex flex-col lg:flex-row w-full h-full gap-4 overflow-auto">
+                        <div className="w-full lg:w-1/2">
+                            <AttendanceCalendar />
+                        </div>
+                        <div className="w-full lg:w-1/2 flex flex-col gap-4">
+                            <div className="w-full">
+                                <SampleCard />
+                            </div>
+                            <div className="w-full">
+                                <ScheduleForm />
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
