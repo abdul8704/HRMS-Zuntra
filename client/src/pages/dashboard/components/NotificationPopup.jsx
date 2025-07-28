be8416021c7cd3cca7d1e7d68fa0652db35629ca
Keerthika <nkeerthika09@gmail.com>
2025-07-28 10:38:58 +0530

    Notification gmeet round border added

--- client/src/pages/dashboard/components/NotificationPopup.jsx
+++ client/src/pages/dashboard/components/NotificationPopup.jsx
@@ -283,7 +283,7 @@ export const NotificationPopup = ({ setShowPopup }) => {
         </div>
 
         {/* Footer */}
-        <div className="flex justify-center space-x-3 p-4 border-t bg-gray-50">
+        <div className="flex justify-center space-x-3 p-4 border-t bg-gray-50 rounded-b-lg">
           <button
             onClick={handleClose}
             className="px-4 py-2 text-gray-600 bg-white border border-gray-300 rounded-md hover:bg-gray-50 transition-colors"
